5f7add11b67374a9dd6bafaf1bf6b8a8b7d373d7
Mj-ede <mede1@students.kennesaw.edu>
2025-11-29 23:43:51 -0500

    fixing a typo

--- Mine_pastel_budget.py
+++ Mine_pastel_budget.py
@@ -89,7 +89,7 @@ def safe_input_float(prompt: str) -> float:
 
 if __name__ == "__main__":
     print (BANNER)
-    print (f"{Pastel.LAVENDER}Welcome to the your Budget Tracker! 🌈✨{Pastel.END}")
+    print (f"{Pastel.LAVENDER}Welcome to your Budget Tracker! 🌈✨{Pastel.END}")
 # writing the main function to run the budget tracker
 # This will print the banner and a welcome message in lavender color with rainbow and sparkle emojis
 # Going to run the code to see if there are any errors so far
